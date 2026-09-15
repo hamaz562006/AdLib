5bd6e6245fd4e13255e3a9df6eea220f85fbb3e6
hamaz562006 <63976379+hamaz562006@users.noreply.github.com>
2026-09-15 15:18:58 +0330

    style: refactor loading dialog UI and window setup
    Update loading dialog layout to use a card-styled container and configure window attributes for a improved visual presentation and consistent dimming.

--- adlib/src/main/java/com/tqhit/adlib/sdk/ui/dialog/LoadingAdsDialog.kt
+++ adlib/src/main/java/com/tqhit/adlib/sdk/ui/dialog/LoadingAdsDialog.kt
@@ -1,7 +1,12 @@
 package com.tqhit.adlib.sdk.ui.dialog
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.graphics.drawable.toDrawable
+import com.tqhit.adlib.R
 import com.tqhit.adlib.databinding.DialogPrepairLoadingAdsBinding
 import com.tqhit.adlib.sdk.base.ui.AdLibBaseDialog
 
@@ -13,5 +18,16 @@ class LoadingAdsDialog(context: Context) : AdLibBaseDialog<DialogPrepairLoadingA
     init {
         setCancelable(false)
         setCanceledOnTouchOutside(false)
+    }
+
+    override fun initWindow() {
+        window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+        window?.attributes?.windowAnimations = R.style.DialogAnimation
+        window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        val layoutParams = WindowManager.LayoutParams()
+        layoutParams.copyFrom(window?.attributes)
+        layoutParams.dimAmount = 0.6f
+        window?.attributes = layoutParams
     }
 }

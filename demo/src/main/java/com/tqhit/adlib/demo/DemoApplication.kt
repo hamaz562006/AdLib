93000d78c7a259a86241a7d4353a668fa34eef71
hamaz562006 <63976379+hamaz562006@users.noreply.github.com>
2026-09-19 10:40:03 +0330

    feat: pass Constant.TEST_DEVICE_IDS through to initAll so physical test devices get registered once populated

--- demo/src/main/java/com/tqhit/adlib/demo/DemoApplication.kt
+++ demo/src/main/java/com/tqhit/adlib/demo/DemoApplication.kt
@@ -2,12 +2,13 @@ package com.tqhit.adlib.demo
 
 import com.tqhit.adlib.R
 import com.tqhit.adlib.sdk.AdLibHiltApplication
+import com.tqhit.adlib.sdk.utils.Constant
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class DemoApplication : AdLibHiltApplication() {
     override fun onCreateExt() {
         super.onCreateExt()
-        initAll(R.xml.remote_config_defaults)
+        initAll(R.xml.remote_config_defaults, testDeviceIds = Constant.TEST_DEVICE_IDS)
     }
 }
